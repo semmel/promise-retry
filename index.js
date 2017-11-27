b85b174b3873967e1ced3a79ef2b0e468bedf30b
Matthias Seemann <seemann@visisoft.de>
2017-11-27 21:32:27 +0100

    style (source): apply own JS code style

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-var factory = function (createExecutor) {
-    return function (settings) {
-        return function (fn) {
-            return function () {
-                return new Promise(createExecutor(fn, settings));
-            };
-        };
-    };
+var factory = function (createExecutor)
+{
+	return function (settings)
+	{
+		return function (fn)
+		{
+			return function ()
+			{
+				return new Promise(createExecutor(fn, settings));
+			};
+		};
+	};
 };
 
-var OutOfRetriesError = function (settings, fn, errors) {
-    this.message = 'Maximum retries count reached';
-    this.settings = settings;
-    this.fn = fn;
-    this.errors = errors;
+var OutOfRetriesError = function (settings, fn, errors)
+{
+	this.message = 'Maximum retries count reached';
+	this.settings = settings;
+	this.fn = fn;
+	this.errors = errors;
 };
 OutOfRetriesError.prototype = Object.create(Error.prototype);
 
-var promiseRetry = factory(function (fn, settings) {
-    var failureCount = 0,
-        errors = [];
-
-    if (!settings || typeof settings.retries !== 'number') {
-        throw new Error("settings.retries must be a number");
-    }
-
-    var getDelay = typeof settings.delay === 'function' ? settings.delay : function(){
-        return settings.delay;
-    };
-
-    function executor(resolve, reject) {
-        return fn()
-            .then(resolve)
-            .catch(function (err) {
-                errors = errors.concat([err]);
-                failureCount++;
-                if (failureCount > settings.retries) {
-                    reject(new OutOfRetriesError(settings, fn, errors));
-                } else {
-                    setTimeout(function () {
-                        executor(resolve, reject);
-                    }, getDelay(failureCount));
-                }
-            });
-    }
-
-    return executor;
+var promiseRetry = factory(function (fn, settings)
+{
+	var failureCount = 0,
+	    errors       = [];
+	
+	if (!settings || typeof settings.retries !== 'number')
+	{
+		throw new Error("settings.retries must be a number");
+	}
+	
+	var getDelay = typeof settings.delay === 'function' ? settings.delay : function ()
+	{
+		return settings.delay;
+	};
+	
+	function executor(resolve, reject)
+	{
+		return fn()
+		.then(resolve)
+		.catch(function (err)
+		{
+			errors = errors.concat([err]);
+			failureCount++;
+			if (failureCount > settings.retries)
+			{
+				reject(new OutOfRetriesError(settings, fn, errors));
+			}
+			else
+			{
+				setTimeout(function ()
+				{
+					executor(resolve, reject);
+				}, getDelay(failureCount));
+			}
+		});
+	}
+	
+	return executor;
 });
 
 
